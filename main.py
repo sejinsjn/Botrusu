43704202ef81c018185c9ef47493e475f5ffb9cb
Sedo <76414770+sejinsjn@users.noreply.github.com>
2022-09-12 20:23:14 +0000

    Event, Redeem and Info posts; One Thread per sub

--- main.py
+++ main.py
@@ -12,12 +12,17 @@ reddit = praw.Reddit(
   user_agent = "<Botrusu>"
 )
 
-ptrades_event_hook = Webhook("https://discord.com/api/webhooks/1018654699194425445/H81oXFw2_qYqaAmiBhamtY2DghFh3UgwCIhRlrJtsdzVV3kEm_6x4tR1XRqn9iZ3z5hd")
+event_hook = Webhook("https://discord.com/api/webhooks/1018967006764081162/tKCCPstVLrdaEoRpljmBTeEf-dSOByiMykOs9jCHwXGRgBr2-iY9KkZEFphy6BqF5r5y")
 
-ptrades_redeem_hook = Webhook("https://discord.com/api/webhooks/1018943521694163036/8uY04Wn3L3ZT72CRE6df7LJNW9iPNKrZIXwls61sespzkmNSYdmolrt1r98WeDf5I8ZR")
+redeem_hook = Webhook("https://discord.com/api/webhooks/1018943521694163036/8uY04Wn3L3ZT72CRE6df7LJNW9iPNKrZIXwls61sespzkmNSYdmolrt1r98WeDf5I8ZR")
+
+info_hook = Webhook("https://discord.com/api/webhooks/1018977913128894575/iNhufVdDb3WvK4NDylDOKFl-UnUMol7KyZB8yDChvEjKVCKax-MSZvcN_3GdeTwSkPaR")
+
+pex_hook = Webhook("https://discord.com/api/webhooks/1018978349386838037/5jtH6Zlx_f-qpLcIaN8fz5m75a8a51plvXUdCOWSmLtoExLKz25kpB1fxQk0Ekr-rbcm")
 
 
 ptrades = reddit.subreddit("pokemontrades")
+pex = reddit.subreddit("Pokemonexchange")
 
 keep_alive()
 
@@ -33,33 +38,32 @@ def get_comments():
     hook.send(embed=embed)
 """
 
-def get_event_submissions_ptrades():
+def get_ptrades():
   for submission in ptrades.stream.submissions(skip_existing=True):
     if(str(submission.link_flair_text) == "Event"):
-      embed = Embed(
-        title=submission.title,
-        description=submission.selftext,
-        timestamp='now',
-        url=submission.url
-      )
-      embed.set_author(name=str(submission.author))
-      ptrades.send(embed=embed)
-
-def get_redeem_submissions_ptrades():
-  for submission in ptrades.stream.submissions(skip_existing=True):
+      event_hook.send(embed=create_embed(submission))
     if(str(submission.link_flair_text) == "Redeem"):
-      embed = Embed(
-        title=submission.title,
-        description=submission.selftext,
-        timestamp='now',
-        url=submission.url
-      )
-      embed.set_author(name=str(submission.author))
-      ptrades.send(embed=embed)
+      redeem_hook.send(embed=create_embed(submission))
+    if(str(submission.link_flair_text) == "Info"):
+      redeem_hook.send(embed=create_embed(submission))
 
+def get_pex():
+  for submission in pex.stream.submissions(skip_existing=True):
+    if(str(submission.link_flair_text) == "Selling Virtual"):
+      pex_hook.send(embed=create_embed(submission))
 
-event = threading.Thread(target=get_event_submissions_ptrades) 
-redeem = threading.Thread(target=get_redeem_submissions_ptrades)
+def create_embed(submission):
+  embed = Embed(
+    title=submission.title,
+    description=submission.selftext,
+    timestamp='now',
+    url=submission.url
+  )
+  embed.set_author(name=str(submission.author))
+  return embed
+      
+ptrades_thread = threading.Thread(target=get_ptrades)
+pex_thread = threading.Thread(target=get_ptrades)
 
-event.start()
-redeem.start()
+ptrades_thread.start()
+pex_thread.start()
