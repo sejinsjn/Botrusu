bfbcbed56004857a0e4ba1cf394264059391a9b1
Sedo <76414770+sejinsjn@users.noreply.github.com>
2022-09-13 07:38:39 +0000

    Webhook to stalk bad ppl

--- main.py
+++ main.py
@@ -20,23 +20,42 @@ info_hook = Webhook("https://discord.com/api/webhooks/1018977913128894575/iNhufV
 
 pex_hook = Webhook("https://discord.com/api/webhooks/1018978349386838037/5jtH6Zlx_f-qpLcIaN8fz5m75a8a51plvXUdCOWSmLtoExLKz25kpB1fxQk0Ekr-rbcm")
 
+stalker_hook = Webhook("https://discord.com/api/webhooks/1018984309748662323/eqkqrasXG4IVSrjEjKjjYjWBlb49uVSTlbhpufK5lGV1n0O1naddFC5NHMz_p2bth5yt")
+
 
 ptrades = reddit.subreddit("pokemontrades")
 pex = reddit.subreddit("Pokemonexchange")
 
+test = reddit.subreddit("memes")
+
 keep_alive()
 
-"""
+def create_embed(submission):
+  embed = Embed(
+    title=submission.title,
+    description=submission.selftext,
+    timestamp='now',
+    url=submission.url
+  )
+  embed.set_author(name=str(submission.author))
+  return embed
+
+def create_comment_embed(comment):
+  embed = Embed(
+    title=str(comment.author),
+    description=str(comment.body),
+    timestamp='now',
+    url="https://www.reddit.com/" + comment.permalink
+  )
+  return embed
+
+bad_ppl = ["Theduskwolf", "sejin_mb", "serenity-as-ice", "KaitoGL"]
+
 def get_comments():
-  for comment in subreddit.stream.comments(skip_existing=True):
-    embed = Embed(
-      description=comment.body,
-      timestamp='now',
-      author=comment.author,
-      url=comment.permalink
-    )
-    hook.send(embed=embed)
-"""
+  for comment in ptrades.stream.comments(skip_existing=True):
+    for bad in bad_ppl:
+      if bad == str(comment.author):
+        stalker_hook.send(embed=create_comment_embed(comment))
 
 def get_ptrades():
   for submission in ptrades.stream.submissions(skip_existing=True):
@@ -51,19 +70,11 @@ def get_pex():
   for submission in pex.stream.submissions(skip_existing=True):
     if(str(submission.link_flair_text) == "Selling Virtual"):
       pex_hook.send(embed=create_embed(submission))
-
-def create_embed(submission):
-  embed = Embed(
-    title=submission.title,
-    description=submission.selftext,
-    timestamp='now',
-    url=submission.url
-  )
-  embed.set_author(name=str(submission.author))
-  return embed
       
 ptrades_thread = threading.Thread(target=get_ptrades)
-pex_thread = threading.Thread(target=get_ptrades)
+pex_thread = threading.Thread(target=get_pex)
+comments_thread = threading.Thread(target=get_comments)
 
 ptrades_thread.start()
 pex_thread.start()
+comments_thread.start()
