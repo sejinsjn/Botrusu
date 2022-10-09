b40d7b9c302afa9e10d7910f8bed1b523305b3b2
Sedo <76414770+sejinsjn@users.noreply.github.com>
2022-10-09 20:28:52 +0000

    Using discord.py instead of WebHooks

--- main.py
+++ main.py
@@ -1,78 +1,167 @@
-import praw
-import threading 
+from discord.ext import tasks
+from discord.ext import commands
+import discord
 import os
-from dhooks import Webhook, Embed
-from keep_alive import keep_alive
+import asyncpraw
+from datetime import datetime
+from replit import db
 
-reddit = praw.Reddit(
-  client_id = os.getenv("client_id"),
-  client_secret = os.getenv("client_secret"),
-  username = os.getenv("username"),
-  password = os.getenv("password"),
-  user_agent = "<Botrusu>"
-)
+TOKEN = os.getenv('token')
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='/', intents=intents)
 
-event_hook = Webhook("https://discord.com/api/webhooks/1018967006764081162/tKCCPstVLrdaEoRpljmBTeEf-dSOByiMykOs9jCHwXGRgBr2-iY9KkZEFphy6BqF5r5y")
+db["annoyuser"] = "nobody"
+db["ssuser"] = "nobody"
+#db["stalk"] = ["Theduskwolf", "KaitoGL", "SwiftGoten", "hgem0406", "sparrowcount", "serenity-as-ice", "bwo0", "OnePointPi", "CrazyNek0"]
 
-redeem_hook = Webhook("https://discord.com/api/webhooks/1018943521694163036/8uY04Wn3L3ZT72CRE6df7LJNW9iPNKrZIXwls61sespzkmNSYdmolrt1r98WeDf5I8ZR")
+#commands
+@bot.command(pass_context=True)
+async def annoy(ctx, arg):
+  db["annoyuser"] = arg.replace(">", "").replace("<", "").replace("@", "").replace(" ", "").strip()
 
-info_hook = Webhook("https://discord.com/api/webhooks/1018977913128894575/iNhufVdDb3WvK4NDylDOKFl-UnUMol7KyZB8yDChvEjKVCKax-MSZvcN_3GdeTwSkPaR")
+@bot.command(pass_context=True)
+async def samsungsmile(ctx, arg):
+  db["ssuser"] = arg.replace(">", "").replace("<", "").replace("@", "").replace(" ", "").strip()
 
-pex_hook = Webhook("https://discord.com/api/webhooks/1018978349386838037/5jtH6Zlx_f-qpLcIaN8fz5m75a8a51plvXUdCOWSmLtoExLKz25kpB1fxQk0Ekr-rbcm")
+@bot.command(pass_context=True)
+async def stalk(ctx, arg):
+  if str(arg) == "list":
+    list = '\n'.join(db["stalk"])
+    await ctx.send(list)
+  else:
+    stalk = db["stalk"]
+    stalk.append(str(arg))
+    db["stalk"] = stalk
+    await ctx.send("Im stalking " + arg + " now!")
 
-stalker_hook = Webhook("https://discord.com/api/webhooks/1018984309748662323/eqkqrasXG4IVSrjEjKjjYjWBlb49uVSTlbhpufK5lGV1n0O1naddFC5NHMz_p2bth5yt")
+@bot.command(pass_context=True)
+async def unstalk(ctx, arg):
+  stalk = db["stalk"]
+  for s in stalk:
+    if s == arg:
+      stalk.remove(arg)
+      db["stalk"] = stalk
+      await ctx.send("Im not stalking " + arg + " anymore! :(")
+      return
+  await ctx.send("Who is " + arg + "? :thinkingeyes:")
 
-ptrades = reddit.subreddit("pokemontrades")
-pex = reddit.subreddit("Pokemonexchange")
-test = reddit.subreddit("askreddit")
-
-keep_alive()
-
+@bot.event
+async def on_message(message):
+  if str(message.author.id) == str(db["annoyuser"]):
+    print("Hello there!" + str(message.author.id))
+    await message.reply('Hello there!', mention_author=True)
+  else:
+    if str(message.author.id) == str(db["ssuser"]):
+      print("Samsungsmile: " + str(message.author.id))
+      await message.add_reaction(':samsungsmile:1018574192020553830')
+    else:
+      await bot.process_commands(message)
+  
+#Reddit updates
 def create_embed(submission):
-  embed = Embed(
+  embed = discord.Embed(
     title=submission.title,
     description=submission.selftext,
-    timestamp='now',
+    timestamp=datetime.now(),
     url=submission.url
   )
   embed.set_author(name=str(submission.author))
   return embed
 
 def create_comment_embed(comment):
-  embed = Embed(
+  embed = discord.Embed(
     title=str(comment.author),
     description=str(comment.body),
-    timestamp='now',
-    url="https://www.reddit.com/" + comment.permalink
+    timestamp=datetime.now(),
+    url="https://www.reddit.com" + comment.permalink
   )
   return embed
 
-bad_ppl = ["Theduskwolf", "KaitoGL", "SwiftGoten" "sejin_mb", "serenity-as-ice", "bwo0"]
+@tasks.loop(seconds = 1)
+async def test_comments():
+  reddit = asyncpraw.Reddit(
+    client_id = os.getenv("client_id"),
+    client_secret = os.getenv("client_secret"),
+    user_agent = "<Botrusu>"
+  )
 
-def get_comments():
-  for comment in ptrades.stream.comments(skip_existing=True):
-    for bad in bad_ppl:
-      if bad == str(comment.author):
-        stalker_hook.send(embed=create_comment_embed(comment))
+  subreddit = await reddit.subreddit("AskReddit")
+  async for comment in subreddit.stream.comments(skip_existing=True):
+    channel = bot.get_channel(1019954852173987942)
+    await channel.send(embed=create_comment_embed(comment))
 
-def get_ptrades():
-  for submission in ptrades.stream.submissions(skip_existing=True):
+@tasks.loop(seconds = 1)
+async def test_submissions():
+  reddit = asyncpraw.Reddit(
+    client_id = os.getenv("client_id"),
+    client_secret = os.getenv("client_secret"),
+    user_agent = "<Botrusu>"
+  )
+
+  subreddit = await reddit.subreddit("askreddit")
+  async for submission in subreddit.stream.submissions(skip_existing=True):
+    channel = bot.get_channel(1019954852173987942)
+    await channel.send(embed=create_embed(submission))
+
+@tasks.loop(seconds = 1)
+async def ptrades_posts():
+  reddit = asyncpraw.Reddit(
+    client_id = os.getenv("client_id"),
+    client_secret = os.getenv("client_secret"),
+    user_agent = "<Botrusu>"
+  )
+
+  subreddit = await reddit.subreddit("pokemontrades")
+  async for submission in subreddit.stream.submissions(skip_existing=True):
     if(str(submission.link_flair_text) == "Event"):
-      event_hook.send(embed=create_embed(submission))
+      channel = bot.get_channel(1018966932092895323)
+      await channel.send(embed=create_embed(submission))
     if(str(submission.link_flair_text) == "Redeem"):
-      redeem_hook.send(embed=create_embed(submission))
+      channel = bot.get_channel(1018943497425928302)
+      await channel.send(embed=create_embed(submission))
     if(str(submission.link_flair_text) == "Info"):
-      redeem_hook.send(embed=create_embed(submission))
+      channel = bot.get_channel(1018977894208372777)
+      await channel.send(embed=create_embed(submission))
 
-def get_pex():
-  for submission in pex.stream.submissions(skip_existing=True):
+@tasks.loop(seconds = 1)
+async def pex_posts():
+  reddit = asyncpraw.Reddit(
+    client_id = os.getenv("client_id"),
+    client_secret = os.getenv("client_secret"),
+    user_agent = "<Botrusu>"
+  )
+
+  subreddit = await reddit.subreddit("Pokemonexchange")
+  async for submission in subreddit.stream.submissions(skip_existing=True):
     if(str(submission.link_flair_text) == "Selling Virtual"):
-      pex_hook.send(embed=create_embed(submission))
+      channel = bot.get_channel(1018978331665903716)
+      await channel.send(embed=create_embed(submission))
 
-ptrades_thread = threading.Thread(target=get_ptrades)
-pex_thread = threading.Thread(target=get_pex)
-comments_thread = threading.Thread(target=get_comments)
+@tasks.loop(seconds = 1)
+async def stalking_ptrades():
+  #["Theduskwolf", "KaitoGL", "SwiftGoten", "hgem0406", "sparrowcount", "sejin_mb", "serenity-as-ice", "bwo0", "OnePointPi", "CrazyNek0"]
+  
+  reddit = asyncpraw.Reddit(
+    client_id = os.getenv("client_id"),
+    client_secret = os.getenv("client_secret"),
+    user_agent = "<Botrusu>"
+  )
 
-ptrades_thread.start()
-pex_thread.start()
-comments_thread.start()
+  subreddit = await reddit.subreddit("pokemontrades")
+  async for comment in subreddit.stream.comments(skip_existing=True):
+    bad_ppl = db["stalk"]
+    for bad in bad_ppl:
+      print(bad)
+      if str(bad) == str(comment.author):
+        channel = bot.get_channel(1018984167284949023)
+        await channel.send(embed=create_comment_embed(comment))
+
+@bot.event
+async def on_ready():
+  ptrades_posts.start()
+  pex_posts.start()
+  stalking_ptrades.start()
+  #test_submissions.start()
+  #test_comments.start()
+
+bot.run(TOKEN)
